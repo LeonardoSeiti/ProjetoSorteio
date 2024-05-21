81bbf66bfe8620bd4d3b6c638db0afb08fca75b7
LeoSeiti <Leonardoseiti1@gmail.com>
2024-05-21 11:14:10 -0300

    Pagina inicial

--- src/main/java/br/com/fiap/sorteio/sorteio/SorteioApplication.java
+++ src/main/java/br/com/fiap/sorteio/sorteio/SorteioApplication.java
@@ -3,6 +3,8 @@ package br.com.fiap.sorteio.sorteio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -24,6 +26,8 @@ public class SorteioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SorteioApplication.class, args);
 	}
+	@RequestMapping
+	@ResponseBody
 	public String home() {
 		return "Bem vindo ao projeto sorteio!, para mais informações acesse a documentação da API em http://localhost:8080/swagger-ui/index.html#/Dados/docs";
 	}
